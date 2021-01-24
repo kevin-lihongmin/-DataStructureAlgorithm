7d460c5af57451aa62e114707ff3504f7bbd08ac
lihongmin@quanyou.com.cn <password>
2021-01-24 17:02:16 +0800

    初始化项目

---
+++ src/main/java/com/kevin/DataStructureAlgorithm/DataStructureAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.DataStructureAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAlgorithmApplication.class, args);
+	}
+
+}
