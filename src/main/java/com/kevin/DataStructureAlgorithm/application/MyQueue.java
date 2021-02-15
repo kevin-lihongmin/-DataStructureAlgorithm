03a9f10f8ae539a9f1f7dcebd0e26c72d76dd822
lihongmin <it_lihongmin@163.com>
2021-02-15 19:57:18 +0800

    用栈实现队列

---
+++ src/main/java/com/kevin/DataStructureAlgorithm/application/MyQueue.java
@@ -0,0 +1,54 @@
+package com.kevin.DataStructureAlgorithm.application;
+
+import java.util.Stack;
+
+/**
+ *  1、LeetCode-232 用栈实现队列
+ *
+ *
+ * @author kevin
+ * @date 2021/2/15 19:53
+ * @since 1.0.0
+ */
+public class MyQueue {
+
+    private static Stack<Integer> input = null;
+
+    private static Stack<Integer> output = null;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        input = new Stack<Integer>();
+        output = new Stack<Integer>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
