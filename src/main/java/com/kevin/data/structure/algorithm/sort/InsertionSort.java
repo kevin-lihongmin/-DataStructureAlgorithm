01274488586529987fbb316ad884c0c351c800c0
lihongmin@quanyou.com.cn <password>
2021-03-03 08:47:41 +0800

    添加二分插入排序

--- src/main/java/com/kevin/data/structure/algorithm/sort/InsertionSort.java
+++ src/main/java/com/kevin/data/structure/algorithm/sort/InsertionSort.java
@@ -8,9 +8,7 @@ import java.util.Random;
  *
  *  插入排序是经常使用的排序算法，其优于冒泡排序， 而选择排序本身是非稳定排序所以使用场景更少
  *
- *  插入排序在数据交换时更加的复杂，所以同样为 O(N²)的时间复杂度，但是插入排序的性能优于冒泡排序
- *
- *  做一个十万的数组，相同的机器配置，执行 {@link InsertionSort#insertionSortVsBubbleSort()} ，执行结果为：
+ *  做不同量级的数组，相同的机器配置，执行 {@link InsertionSort#insertionSortVsBubbleSort()} ，执行结果为：
  *  arrayByInsertion length = 100000 排序花费时间为：765
  *  arrayByBubble length = 100000 排序花费时间为：14366
  *
@@ -20,7 +18,7 @@ import java.util.Random;
  *                             冒泡排序        插入排序
  * 一万性能差【9】倍              155           17
  * 十万性能差【20】倍             765（或1017）  14366（或19166）
- * 百万耗时太长，我等不到结构
+ * 百万耗时太长，我等不到结果
  *
  * @author kevin
  * @date 2021/2/21 23:53
@@ -52,13 +50,12 @@ public class InsertionSort {
 
     }
 
-    public static void main(String[] args) {
-        insertionSortVsBubbleSort();
-        /*int[] array = new int[]{12, 34, -34, 454, 657, 33, 89, 67, 68, 99, -23, 34};
-        System.out.println("排序前：" + Arrays.toString(array));
-        insertionSort(array);
-        System.out.println("排序后：" + Arrays.toString(array));*/
-    }
+public static void main(String[] args) {
+    int[] array = new int[]{12, 34, -34, 454, 657, 33, 89, 67, 68, 99, -23, 34};
+    System.out.println("排序前：" + Arrays.toString(array));
+    insertionSort(array);
+    System.out.println("排序后：" + Arrays.toString(array));
+}
 
 /**
  * 插入排序
